aaa0294d2aa24802e7a27b53ac8b938b3649aa79
Kasey Markham <kaseycm@gmail.com>
2017-05-14 23:09:06 -0400

    Spec error fix attempt

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -2,18 +2,9 @@ require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
 
-  describe "GET #index" do
-    it "returns http success" do
+  it "redirects to signin" do
       get :index
-      expect(response).to have_http_status(:success)
+      expect(response).to redirect_to( new_user_session_path )
     end
-  end
-
-  describe "GET #about" do
-    it "returns http success" do
-      get :about
-      expect(response).to have_http_status(:success)
-    end
-  end
 
 end
