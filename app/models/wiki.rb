8329d77dea6fa278a3bf8a3d5dba6a3533cf40ce
Kasey Markham <kaseycm@gmail.com>
2017-05-23 16:14:10 -0400

    Stripe setup

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -2,4 +2,8 @@ class Wiki < ActiveRecord::Base
   belongs_to :user
 
   default_scope { order('created_at DESC') }
+
+  validates :title, length: { minimum: 2 }, presence: true
+  validates :body, length: { minimum: 5 }, presence: true
+  validates :user, presence: true
 end
--- spec/models/wiki_spec.rb
+++ spec/models/wiki_spec.rb
@@ -6,6 +6,13 @@ RSpec.describe Wiki, type: :model do
 
   it { is_expected.to belong_to(:user) }
 
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:body) }
+  it { is_expected.to validate_presence_of(:user) }
+
+  it { is_expected.to validate_length_of(:title).is_at_least(2) }
+  it { is_expected.to validate_length_of(:body).is_at_least(5) }
+
   describe "attributes" do
     it "has title body and user attributes" do
       expect(wiki).to have_attributes(title: "New Wiki Title", body: "New Wiki Body", user: user)
