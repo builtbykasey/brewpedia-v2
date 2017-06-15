4315973cf2df1cee6c8a0d0f3753e2dcab996fee
Kasey Markham <kaseycm@gmail.com>
2017-06-14 22:35:50 -0400

    Signup name display fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,18 @@ class ApplicationController < ActionController::Base
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+        user_params.permit(:name, :email, :password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |user_params|
+        user_params.permit(:name, :email, :password, :password_confirmation, :current_password)
+    end
+  end
 
   def user_not_authorized
     flash[:notice] = "You are not authorized to perform this action."
